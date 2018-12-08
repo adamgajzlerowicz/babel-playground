20450c299d206258c3238e152488de75931a9785
Adam Gajzlerowicz <nelf86@gmail.com>
2018-12-08 09:05:34 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { a, b, c } from './modules'
+
+console.log(a, b, c)
---
+++ src/modules/index.js
@@ -0,0 +1,7 @@
+export a from './a'
+export b from './b'
+
+// regular import export
+
+import c from './c'
+export { c }
